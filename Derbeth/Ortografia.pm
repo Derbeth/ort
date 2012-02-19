e9e312de27d70e87e0bd3e4182a89bd1d4304484
Derbeth <derbeth.fora@wp.pl>
2012-02-19 11:53:22 +0100

    fixed link to the git repository

--- Derbeth/Ortografia.pm
+++ Derbeth/Ortografia.pm
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2007-2008 Derbeth
+# Copyright (c) 2007-2012 Derbeth
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- page/index.php
+++ page/index.php
@@ -213,7 +213,7 @@ document.getElementById('output').select();
 	</script>
 <?php } ?>
 <hr/>
-<p>Autorem skryptu jest <a href="http://pl.wikipedia.org/wiki/Wikipedysta:Derbeth">Derbeth</a>. Program jest Wolnym Oprogramowaniem na <a href="http://www.opensource.org/licenses/mit-license.php" title="MIT license">licencji MIT</a>, jego kod znajduje się w katalogu <a href="script/">script</a>. Autor nie udziela żadnych gwarancji ani rękojmi na jego działanie.</p>
+<p>Autorem skryptu jest <a href="http://pl.wikipedia.org/wiki/Wikipedysta:Derbeth">Derbeth</a>. Program jest Wolnym Oprogramowaniem na <a href="http://www.opensource.org/licenses/mit-license.php" title="MIT license">licencji MIT</a>. <a href="https://github.com/Derbeth/ort">Kod źródłowy</a>. Autor nie udziela żadnych gwarancji ani rękojmi na jego działanie.</p>
 <p><a href="http://validator.w3.org/check?uri=referer"><img
 src="http://www.w3.org/Icons/valid-xhtml10-blue"
 alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
