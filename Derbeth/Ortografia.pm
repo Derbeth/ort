88b718b538bac82ab587b615cbcc382fb592366a
Derbeth <derbeth@interia.pl>
2014-11-05 21:12:36 +0100

    16 [[Megabajt]]owy -> 16-[[megabajt]]owy

--- Derbeth/Ortografia.pm
+++ Derbeth/Ortografia.pm
@@ -29,7 +29,7 @@ use utf8;
 use English;
 
 our @ISA = qw/Exporter/;
-our $VERSION = 0.6.15;
+our $VERSION = 0.6.16;
 my @EXPORT = ('popraw_pisownie');
 
 our $rzymskie_niebezp = 0; # pozwala na niebezpieczne zamiany
@@ -344,7 +344,8 @@ sub popraw_pisownie {
 
 	my $JEDNOSTKI = '((?:tera|giga|mega|kilo|deka|centy|mili|nano)?(?:bajtow|gramow|hercow|metrow)|barwn|biegow|bitow|bramkow|calow|cylindrow|cyfrow|częściow|dekadow|dniow|dolarow|dzieln|dzienn|etapow|fazow|funtow|galonow|godzinn|groszow|gwiazdkow|hektarow|jardow|kanałow|karatow|kątn|klasow|klawiszow|kołow|kondygnacyjn|konn|krotn|lec|letn|lufow|masztow|miejscow|miesięczn|miliardow|milionow|minutow|nabojow|nawow|odcinkow|osobow|palczast|pasmow|piętrow|pinow|płytow|procentow|procesorow|przęsłow|punktow|ramienn|rdzeniow|roczn|rurow|sekundow|setow|silnikow|spadow|stopniow|stronn|strunow|strzałow|suwow|ścienn|taktow|tomow|tonow|tygodniow|tysięczn|uncjow|wartościow|watow|wieczn|woltow|wymiarow|zaworow|zębow)(ia|ie|iu|ią|iej|ych|ymi|ym|ego|emu|ej|[aeyią])|lat(ek|kami|ka|kiem|ki|ku|ków)';
 	my $LICZEBNIKI = '';
-	$linia =~ s/(\d)(?: | - |[-–—] | [-–—]|\. |–|—)($JEDNOSTKI)\b/$1-$2/og; # 32 bitowy -> 32-bitowy
+	$linia =~ s/(\d)(?: | - |[-–—] | [-–—]|\. |–|—)($JEDNOSTKI)\b/$1-\L$2/ogi; # 32 bitowy -> 32-bitowy
+	$linia =~ s/(\d)(?: | - |[-–—] | [-–—]|\. |–|—)(\[\[)((?:tera|giga|mega|kilo|deka|centy|mili|nano)?(?:bajt|bit|herc|metr)|cal|dolar|funt|galon|hektar|jard|karat|wat|wolt)(\]\]ow(ego|emu|ych|ymi|ym|ą|e|a|y))\b/$1-$2\L$3$4/gi;
 	$linia =~ s/\b([dD]wu|[cC]ztero|[pP]ięcio|[sS]ześcio|[sS]iedmio|[oO]śmio|[dD]ziewięcio|[dD]ziesięcio|[dD]wunasto|[pP]iętnasto|[sS]zesnasto|[dD]wudziesto|[tT]rzydziesto) i pół ($JEDNOSTKI)/$1ipół$2/og; # http://so.pwn.pl/zasady.php?id=629465
 	$linia =~ s/\b([jJ]edno|[dD]wu|[tT]rój|[tT]rzy|[cC]ztero|[pP]ięcio|[sS]ześcio|[sS]iedmio|[oO]śmio|[dD]ziewięcio|[dD]ziesięcio|[dD]wunasto|[pP]iętnasto|[sS]zesnasto|[dD]wudziesto|[pP]ółtora|[pP]ół|[tT]rzydziesto|[sS]tu|[wW]ielo)(?: | - |[-–—] | [-–—]|\. |–|—|-)($JEDNOSTKI)\b/$1$2/og; # sześcio tonowy -> sześciotonowy
 	$linia =~ s/\b([dD]wu|[cC]ztero|[pP]ięcio|[sS]ześcio|[sS]iedmio|[oO]śmio|[dD]ziewięcio|[dD]ziesięcio|[dD]wunasto|[pP]iętnasto|[sS]zesnasto|[dD]wudziesto|[tT]rzydziesto)-(lub)/$1- $2/og; # trzy-lub czterokołowy
